4a98a7c479e3a77523183f5d23775962345c1e5e
Nick Crow <nicholas@nicholasjcrow.com>
2014-11-24 14:57:38 -0800

    Nicer GUI
    Made the gui look nicer and removed unneeded variables

--- Class Maker/Form1.Designer.vb
+++ Class Maker/Form1.Designer.vb
@@ -24,39 +24,91 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(391, 22)
+        Me.TextBox1.Location = New System.Drawing.Point(6, 19)
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(543, 222)
+        Me.TextBox1.Size = New System.Drawing.Size(394, 197)
         Me.TextBox1.TabIndex = 0
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(59, 44)
+        Me.Button1.Location = New System.Drawing.Point(993, 211)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "Start!"
         Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(6, 19)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox2.Size = New System.Drawing.Size(1050, 94)
+        Me.TextBox2.TabIndex = 2
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 240)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(1062, 121)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Status"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.ListBox1)
+        Me.GroupBox2.Controls.Add(Me.TextBox1)
+        Me.GroupBox2.Location = New System.Drawing.Point(18, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(957, 222)
+        Me.GroupBox2.TabIndex = 4
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Found Classes: "
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(418, 17)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(500, 199)
+        Me.ListBox1.TabIndex = 1
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1086, 314)
+        Me.ClientSize = New System.Drawing.Size(1086, 365)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
     Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
     Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
 
 End Class
--- Class Maker/Form1.vb
+++ Class Maker/Form1.vb
@@ -7,41 +7,10 @@ Imports System.Threading
 Public Class Form1
 #Region "Declarations"
     Public C As New CookieContainer
-    Public Money As Double = 0
-    Public Session As New SessionInfo
-    Public PagesInSubscribers As Integer = 0
-    Public PagesGoneThrough As Integer = 0
-    Public NumberSubsGrabbed As Integer = 0
+
 #End Region
 #Region "Events"
     Public Event Stats(Status As String)
-#End Region
-#Region "Structures"
-    Public Structure SessionInfo
-        Dim DispatchVar
-        Dim ContextVar
-        Dim AuthVar
-    End Structure
-    Public Structure CustomerInformation
-        Dim Name
-        Dim Email
-        Dim CustomerID
-        Dim StartDate
-        Dim Price
-        Dim Status
-    End Structure
-    Public Structure RecentPayment
-        Dim Name
-        Dim Amount
-        Dim Email
-        Dim RecievedDate
-        Dim Time
-    End Structure
-    Public Structure SessionData 'Structure containing the current Session Data
-        Dim session As String
-        Dim dispatch As String
-        Dim context As String
-    End Structure
 #End Region
 
     Private trd As Thread
@@ -67,47 +36,46 @@ Public Class Form1
 
     End Sub
     Sub checkClasses(ClassCodes As String)
-        'Try
-        RaiseEvent Stats("Checking first classes page")
-        Dim url = "https://act.ucsd.edu/scheduleOfClasses/scheduleOfClassesStudentResult.htm?selectedTerm=WI15&xsoc_term=&loggedIn=false&tabNum=&selectedSubjects=" + ClassCodes + "&_selectedSubjects=1&schedOption1=true&_schedOption1=on&_schedOption11=on&_schedOption12=on&schedOption2=true&_schedOption2=on&_schedOption4=on&_schedOption5=on&_schedOption3=on&_schedOption7=on&_schedOption8=on&_schedOption13=on&_schedOption10=on&_schedOption9=on&schDay=M&_schDay=on&schDay=T&_schDay=on&schDay=W&_schDay=on&schDay=R&_schDay=on&schDay=F&_schDay=on&schDay=S&_schDay=on&schStartTime=12%3A00&schStartAmPm=0&schEndTime=12%3A00&schEndAmPm=0&_selectedDepartments=1&schedOption1Dept=true&_schedOption1Dept=on&_schedOption11Dept=on&_schedOption12Dept=on&schedOption2Dept=true&_schedOption2Dept=on&_schedOption4Dept=on&_schedOption5Dept=on&_schedOption3Dept=on&_schedOption7Dept=on&_schedOption8Dept=on&_schedOption13Dept=on&_schedOption10Dept=on&_schedOption9Dept=on&schDayDept=M&_schDayDept=on&schDayDept=T&_schDayDept=on&schDayDept=W&_schDayDept=on&schDayDept=R&_schDayDept=on&schDayDept=F&_schDayDept=on&schDayDept=S&_schDayDept=on&schStartTimeDept=12%3A00&schStartAmPmDept=0&schEndTimeDept=12%3A00&schEndAmPmDept=0&courses=&sections=&instructorType=begin&instructor=&titleType=contain&title=&_hideFullSec=on&_showPopup=on"
-        Dim request As HttpWebRequest = HttpWebRequest.Create(url)
-        With request
-            .Referer = "https://act.ucsd.edu/scheduleOfClasses/scheduleOfClassesStudent.htm"
-            .UserAgent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.75 Safari/535.7"
-            .KeepAlive = True
-            .CookieContainer = C
-            .Method = "POST"
-            .Timeout = 2000
-            Dim sb As New StringBuilder
+        Try
+            RaiseEvent Stats("Checking first classes page")
+            Dim url = "https://act.ucsd.edu/scheduleOfClasses/scheduleOfClassesStudentResult.htm?selectedTerm=WI15&xsoc_term=&loggedIn=false&tabNum=&selectedSubjects=" + ClassCodes + "&_selectedSubjects=1&schedOption1=true&_schedOption1=on&_schedOption11=on&_schedOption12=on&schedOption2=true&_schedOption2=on&_schedOption4=on&_schedOption5=on&_schedOption3=on&_schedOption7=on&_schedOption8=on&_schedOption13=on&_schedOption10=on&_schedOption9=on&schDay=M&_schDay=on&schDay=T&_schDay=on&schDay=W&_schDay=on&schDay=R&_schDay=on&schDay=F&_schDay=on&schDay=S&_schDay=on&schStartTime=12%3A00&schStartAmPm=0&schEndTime=12%3A00&schEndAmPm=0&_selectedDepartments=1&schedOption1Dept=true&_schedOption1Dept=on&_schedOption11Dept=on&_schedOption12Dept=on&schedOption2Dept=true&_schedOption2Dept=on&_schedOption4Dept=on&_schedOption5Dept=on&_schedOption3Dept=on&_schedOption7Dept=on&_schedOption8Dept=on&_schedOption13Dept=on&_schedOption10Dept=on&_schedOption9Dept=on&schDayDept=M&_schDayDept=on&schDayDept=T&_schDayDept=on&schDayDept=W&_schDayDept=on&schDayDept=R&_schDayDept=on&schDayDept=F&_schDayDept=on&schDayDept=S&_schDayDept=on&schStartTimeDept=12%3A00&schStartAmPmDept=0&schEndTimeDept=12%3A00&schEndAmPmDept=0&courses=&sections=&instructorType=begin&instructor=&titleType=contain&title=&_hideFullSec=on&_showPopup=on"
+            Dim request As HttpWebRequest = HttpWebRequest.Create(url)
+            With request
+                .Referer = "https://act.ucsd.edu/scheduleOfClasses/scheduleOfClassesStudent.htm"
+                .UserAgent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.75 Safari/535.7"
+                .KeepAlive = True
+                .CookieContainer = C
+                .Method = "POST"
+                .Timeout = 2000
+                Dim sb As New StringBuilder
 
-            Dim byteArray As Byte() = Encoding.UTF8.GetBytes(sb.ToString)
-            .ContentLength = byteArray.Length
-            Dim dataStream As Stream = .GetRequestStream()
-            dataStream.Write(byteArray, 0, byteArray.Length)
-            dataStream.Close() : dataStream.Dispose() : dataStream = Nothing
+                Dim byteArray As Byte() = Encoding.UTF8.GetBytes(sb.ToString)
+                .ContentLength = byteArray.Length
+                Dim dataStream As Stream = .GetRequestStream()
+                dataStream.Write(byteArray, 0, byteArray.Length)
+                dataStream.Close() : dataStream.Dispose() : dataStream = Nothing
 
-            Dim response As System.Net.HttpWebResponse = .GetResponse
+                Dim response As System.Net.HttpWebResponse = .GetResponse
 
-            Dim sr As System.IO.StreamReader = New System.IO.StreamReader(response.GetResponseStream())
-            Dim dataresponse As String = sr.ReadToEnd
-            RaiseEvent Stats("Grabbed a page of classes.")
-            displayMatches(dataresponse)
-        End With
+                Dim sr As System.IO.StreamReader = New System.IO.StreamReader(response.GetResponseStream())
+                Dim dataresponse As String = sr.ReadToEnd
+                RaiseEvent Stats("Grabbed a page of classes.")
+                displayMatches(dataresponse)
+            End With
 
-        Pageinate(2)
-        'Catch ex As Exception
-        'TextBox1.AppendText(ex.Message)
-        'End Try
+            Pageinate(2)
+        Catch ex As Exception
+            RaiseEvent Stats(ex.Message)
+        End Try
 
     End Sub
     Sub displayMatches(data As String)
         RaiseEvent Stats("Displaying matches:")
         Dim delim As String() = New String(0) {"title=""Final"">FI</span>"}
-        'Split = temp_string.Split(delim, StringSplitOptions.None)
+
         Dim splitByClass = data.Split(delim, StringSplitOptions.RemoveEmptyEntries)
-        'TextBox1.AppendText(splitByClass(0))
         For i = 0 To Convert.ToInt32(splitByClass.Length) - 2
-            'RaiseEvent Stats(i & " " & Convert.ToInt32(splitByClass.Length) - 2)
+
             Dim classNameRegex As New Regex("<[a-zA-Z0-9\s]*=""javascript:[a-zA-Z]*\('http:[\/a-zA-Z\.]*.ucsd.edu\/catalog\/courses\/[a-zA-Z0-9]*.html\#([a-zA-Z\.0-9]*)'\)""><[a-zA-Z0-9\s=""]*>([0-9a-zA-Z\/\s-:\(\)\.&;]*)") '<[a-zA-Z0-9]*\s[a-zA-Z0-9=""]*javascript:openNewWindow[a-zA-Z\(\)\':/\.\#0-9""]*><[a-zA-Z\(\)\':/\.\#0-9""\s=]*>([a-zA-Z\(\)\':/\.\#0-9\""\s=\.&;-]*)<\/span>") '
             Dim classCountRegex As New Regex("<td\sclass=[""a-zA-Z]*>([0-9]*)<\/td>\s*<td\sclass=[""a-zA-Z]*>([0-9]*)<\/td>\s*[0-9a-zA-Z<\s="">:;]*\('https:\/\/ucsdbkst.ucsd.edu\/wrtx\/TextSearch\?[a-zA-Z]*=([0-9]*)") ''<[a-zA-Z0-9\s=]*"[a-zA-Z0-9]*">([0-9a-zA-Z\(\)]*)<\/[0-9a-zA-Z>\s]*<[a-zA-Z0-9\s=]*"[a-zA-Z0-9]*">([0-9a-zA-Z\(\)]*)<\/[0-9a-zA-Z>\s]*<[a-zA-Z0-9\s=]*"[a-zA-Z0-9]*"><[a-zA-Z\s=":]*;"\s*onclick=[0-9a-zA-Z":]*\('https:\/\/ucsdbkst.ucsd.edu\/wrtx\/TextSearch\?section=
 
@@ -117,7 +85,7 @@ Public Class Form1
             For Each Match As Match In classCountMatches
                 seatsAvailable += Match.Groups(1).ToString()
             Next Match
-            TextBox1.AppendText(classNameMatches(0).Groups(1).ToString().Replace("&amp;", " ") & " " & classNameMatches(0).Groups(2).ToString().Replace("&amp;", " ") & " has " & seatsAvailable & " seats available." & vbNewLine)
+            ListBox1.Items.Add(classNameMatches(0).Groups(1).ToString().Replace("&amp;", " & ") & classNameMatches(0).Groups(2).ToString().Replace("&amp;", " &") & " has " & seatsAvailable & " seats available." & vbNewLine)
 
 
         Next i
@@ -165,6 +133,6 @@ Public Class Form1
 
 
     Private Sub Form1_Stats(Status As String) Handles Me.Stats
-        TextBox1.AppendText(Status & vbNewLine)
+        TextBox2.AppendText(Status & vbNewLine)
     End Sub
 End Class
